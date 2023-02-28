97c04262e475f55acd08b695b78aa708e6cd4df1
SaraG94 <sara.gherbassi.94@gmail.com>
2023-02-28 22:34:25 +0100

    FEAT components +style

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,24 @@
 <script>
+
+import AppTitle from './components/AppTitle.vue'
+
 export default{
+
+  components:{
+    AppTitle
+  },
+
   data(){
     return {
-      
+      message:'ciao'
     }
-  },
-  methods:{
-
   }
 }
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-
+  
+  <AppTitle></AppTitle>
 </template>
 
 <style scoped>
--- src/components/AppTitle.vue
+++ src/components/AppTitle.vue
@@ -9,11 +9,17 @@ export default{
 </script>
 
 <template>
-    <h1>
+    <h1 class="main-title">
         {{title}}
     </h1>
 </template>
 
 <style>
 
+.main-title{
+    background-color: #9cbfb3;
+    border-radius: 15px;
+    padding: 10px 15px;
+    color: white;
+}
 </style>
